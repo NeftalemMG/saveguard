a01b0222b55493198b5431bf0b9f8c3c9ac7a46e
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2026-03-18 14:53:42 -0400

    Adding apinotice to the page

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Features from '@/components/Features'
 import HowItWorks from '@/components/HowItWorks'
 import Stats from '@/components/Stats'
 import Footer from '@/components/Footer'
+import ApiNotice from '@/components/ApiNotice'
 
 export default function Home() {
   return (
     <main>
+      <ApiNotice /> 
       <Navigation />
       <Hero />
       <Features />
